95f327927ec8d2504631d6078b38d4d8af37f723
e-nk <enocklagat747@gmail.com>
2024-11-05 10:16:27 +0300

    add reactjs and jsbundling to our codebase. jsbundling-rails and esbuild will help build our jsx files into javascript

---
+++ db/migrate/20241104162811_add_places_table.rb
@@ -0,0 +1,11 @@
+class AddPlacesTable < ActiveRecord::Migration[7.2]
+  def change
+    create_table :places, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :city, null: false
+
+      t.timestamps
+    end
+  end
+end
